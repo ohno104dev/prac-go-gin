775dc04f352c26c9159a50bb06900b4d52c6be05
ohno <ohno104dev@gmail.com>
2024-11-08 00:39:46 +0800

    feat: next and abort of gin middleware

--- gin_mw/main.go
+++ gin_mw/main.go
@@ -11,9 +11,10 @@ import (
 func timeMW() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		begin := time.Now()
-		ctx.Next()
+		ctx.Next() // 往下一個執行串列調用
 		elapsed := time.Since(begin)
 		log.Printf("%s use time %d ms\n", ctx.Request.URL.Path, elapsed.Milliseconds())
+		ctx.String(200, " hihihi")
 	}
 }
 
@@ -22,17 +23,21 @@ var limitCh = make(chan struct{}, 10)
 func LimitMW() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		limitCh <- struct{}{}
+		log.Printf("concurrence %d\n", len(limitCh))
 		ctx.Next()
 		<-limitCh
 	}
 }
 
+func boy(ctx *gin.Context) {
+	ctx.String(http.StatusOK, "gin, hi boy")
+	ctx.Abort() // 中止往後的串列調用
+}
+
 func main() {
 	engine := gin.Default()
-	engine.Use(LimitMW()) //global middlewares
-	engine.GET("/boy", timeMW(), func(ctx *gin.Context) {
-		ctx.String(http.StatusOK, "gin, hi boy")
-	})
+	// engine.Use(LimitMW()) //global middlewares
+	engine.GET("/boy", timeMW(), boy, LimitMW())
 
 	engine.Run("127.0.0.1:8000")
 }
